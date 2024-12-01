1d843661a21e2d4a3a9c07b6545cc134f5a605e3
Acrylic125 <acrylic125email@gmail.com>
2024-12-01 16:18:08 +0800

    chore: add c++ lsp

--- lua/ben/plugins-setup.lua
+++ lua/ben/plugins-setup.lua
@@ -1,14 +1,14 @@
 -- https://www.youtube.com/watch?v=vdn_pKJUda8
 
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -23,87 +23,94 @@ vim.cmd([[
 -- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
 -- TODO: Change the path to the absolute path of the venv
 -- comprising your neovim's python deps.
 local python_venv_path = "/Users/benedicttan/.config/nvim/python/neovim/bin/python3"
-vim.g.python3_host_prog=vim.fn.expand(python_venv_path)
+vim.g.python3_host_prog = vim.fn.expand(python_venv_path)
 
 -- add list of plugins to install
 return packer.startup(function(use)
-    use('wbthomason/packer.nvim')
-    use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
-    use("navarasu/onedark.nvim") -- color scheme
-    -- use("folke/tokyonight.nvim") -- color scheme
-    -- use({ 'Everblush/nvim', as = 'everblush' })
-    use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
-    use("szw/vim-maximizer") -- maximizes and restores current window
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+	use("navarasu/onedark.nvim") -- color scheme
+	-- use("folke/tokyonight.nvim") -- color scheme
+	-- use({ 'Everblush/nvim', as = 'everblush' })
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+	use("szw/vim-maximizer") -- maximizes and restores current window
 
-    use("nvim-tree/nvim-tree.lua") -- file explorer
-    use("nvim-tree/nvim-web-devicons")
-    use("nvim-lualine/lualine.nvim")  -- statusline
+	use("nvim-tree/nvim-tree.lua") -- file explorer
+	use("nvim-tree/nvim-web-devicons")
+	use("nvim-lualine/lualine.nvim") -- statusline
 
-    -- fuzzy finding w/ telescope
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+	-- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
-    -- autocompletion
-    use("hrsh7th/nvim-cmp") -- completion plugin
-    use("hrsh7th/cmp-buffer") -- source for text in buffer
-    use("hrsh7th/cmp-path") -- source for file system paths
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
 
-    -- snippets
-    use("L3MON4D3/LuaSnip") -- snippet engine
-    use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-    use("rafamadriz/friendly-snippets") -- useful snippets
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 
-    -- managing & installing lsp servers, linters & formatters
-    use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-    use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
-    use("neovim/nvim-lspconfig") -- easily configure language servers
-    use("hrsh7th/cmp-nvim-lsp") -- for autocompletion 
-    use ({
-       'nvimdev/lspsaga.nvim',
-       after = 'nvim-lspconfig',
-       config = function()
-           require('lspsaga').setup({})
-       end,
-    })
-    use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-    use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+	use("neovim/nvim-lspconfig") -- easily configure language servers
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({
+		"nvimdev/lspsaga.nvim",
+		after = "nvim-lspconfig",
+		config = function()
+			require("lspsaga").setup({})
+		end,
+	})
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-    -- formatting & linting
-    use({
-        "nvimtools/none-ls.nvim",
-        requires = {
-            'nvimtools/none-ls-extras.nvim',
-        },
-    }) -- configure formatters & linters
-    use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	-- formatting & linting
+	use({
+		"nvimtools/none-ls.nvim",
+		requires = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+	}) -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-    -- Jupyter Notebooks
-    -- use({
-    --     "benlubas/molten-nvim",
-    --     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-    --     build = ":UpdateRemotePlugins",
-    -- })
+	-- Jupyter Notebooks
+	-- use({
+	--     "benlubas/molten-nvim",
+	--     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+	--     build = ":UpdateRemotePlugins",
+	-- })
 
-    -- Lush, for custom themes.
-    -- use('rktjmp/lush.nvim')
+	-- Lush, for custom themes.
+	-- use('rktjmp/lush.nvim')
 
-    -- Auto pair 
-    use({
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = function()
-            require("nvim-autopairs").setup {}
-        end
-    })
+	-- Auto pair
+	use({
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	-- Comments
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
--- lua/ben/plugins/lsp/mason.lua
+++ lua/ben/plugins/lsp/mason.lua
@@ -1,41 +1,42 @@
 -- import mason plugin safely
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
 -- import mason-lspconfig plugin safely
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
 -- import mason-null-ls plugin safely
 -- local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 -- if not mason_null_ls_status then
-  -- return
+-- return
 -- end
 
 -- enable mason
 mason.setup()
 
 mason_lspconfig.setup({
-  -- list of servers for mason to install
-  ensure_installed = {
-    "ts_ls",
-    "html",
-    "cssls",
-    "tailwindcss",
-    "lua_ls",
-    "emmet_ls",
-    "gopls",
-    --"sourcekit",
-    "biome",
-    "astro",
-    "pyright",
-  },
-  -- auto-install configured servers (with lspconfig)
-  automatic_installation = true, -- not the same as ensure_installed
+	-- list of servers for mason to install
+	ensure_installed = {
+		"ts_ls",
+		"html",
+		"cssls",
+		"tailwindcss",
+		"clangd",
+		"lua_ls",
+		"emmet_ls",
+		"gopls",
+		--"sourcekit",
+		"biome",
+		"astro",
+		"pyright",
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
 })
 
 -- mason_null_ls.setup({
